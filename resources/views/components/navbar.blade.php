195306cb13e86ad9ef14792e74b6f09a8c46d961
Dino Fajar <70890106+wattaw@users.noreply.github.com>
2022-06-21 14:34:35 +0700

    install and integration socialite

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,3 @@
-
 <nav class="navbar navbar-expand-lg navbar-light">
     <div class="container">
         <a class="navbar-brand" href="{{route('welcome')}}">
@@ -22,6 +21,30 @@
                     <a class="nav-link" href="#">Business</a>
                 </li>
             </ul>
+            @auth
+            <div class="d-flex user-logged nav-item dropdown no-arrow">
+                <a href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                Halo,{{Auth::User()->name}}!
+                <img src="{{Auth::User()->avatar}}" class="user-photo" alt="">
+                <ul class="dropdown" aria-labelledby="dropdownMenuLink" style="right: 0; left: auto">
+                    <li>
+                        <a href="#" class="dropdown-item">My Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="#" class="dropdown-item" onclick="event.preventDefault(); document.getElementById('logout-form').submit()">Sign Out</a>
+                        <form id="logout-form" action="{{route('logout')}}" method="post" style="dispaly: none">
+                            <input type="hidden" name="_token" value="{{csrf_token()}}">
+                        </form>
+                    </li>
+                </ul>
+                </a>
+                {{-- <a href="#" class="btn btn-master btn-secondary me-3">My Dashboard</a>
+                <a href="#" class="btn btn-master btn-primary" onclick="event.preventDefault(); document.getElementById('logout-form').submit()">Sign Out</a>
+                <form id="logout-form" action="{{route('logout')}}" method="post" style="dispaly: none">
+                    <input type="hidden" name="_token" value="{{csrf_token()}}">
+                </form> --}}
+            </div>
+            @else
             <div class="d-flex">
                 <a href="{{route('login')}}" class="btn btn-master btn-secondary me-3">
                     Sign In
@@ -30,6 +53,7 @@
                     Sign Up
                 </a>
             </div>
+            @endauth
         </div>
     </div>
 </nav>
